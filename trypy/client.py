2cad29ef4cf151349d6a4ca67887426b64b408a8
aa257 <53916561+aa257@users.noreply.github.com>
2019-09-10 09:00:21 +0100

    Another bogus commit

--- trypy/client.py
+++ trypy/client.py
@@ -1,7 +1,12 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 20000))
+s.connect(('35.246.81.196', 20000))
+
 rx = repr(s.recv(1024))
 print(rx)
+
+rx = repr(s.recv(1024))
+print(rx)
+
 s.close()
--- trypy/server.py
+++ trypy/server.py
@@ -1,3 +1,4 @@
+import time
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -6,6 +7,13 @@ s.listen(5)
 
 while True:
  c, addr = s.accept()
- c.send(b'Thank you for connecting') 
+ c.send(b'HTTP/1.1 200 OK\n')
+ c.send(b'Content-Type: text/html; charset=utf-8\n')
+ c.send(b'Content-Length: 51\n')
+ c.send(b'\n')
+ c.send(b'<html><body>\n')
+ c.send(b'<h1>Hello, World!</h1>\n')
+ c.send(b'</body></html>\n')
+ time.sleep(5)
  c.close()
 s.close()
